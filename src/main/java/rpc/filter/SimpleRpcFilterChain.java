b7ae35c5d65ff43715e6cc432676cc4f7651873f
Jason Sun <sunb@ece.ubc.ca>
2016-08-08 23:20:15 +0800

    Adding Filter Module

---
+++ src/main/java/rpc/filter/SimpleRpcFilterChain.java
@@ -0,0 +1,53 @@
+package main.java.rpc.filter;
+
+import main.java.rpc.RemoteCall;
+import main.java.rpc.RpcObject;
+import main.java.rpc.network.RpcSender;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bsun on 2016/8/8.
+ */
+public class SimpleRpcFilterChain implements RpcFilterChain {
+
+    private List<RpcFilter> filters = new ArrayList<>();
+
+    private ThreadLocal<Integer> rpcFilterIndex = new ThreadLocal<>();
+
+    private int getAndIncrFilterIndex(){
+        Integer index = rpcFilterIndex.get();
+        if(index == null){
+            index = 0;
+        }
+        rpcFilterIndex.set(index + 1);
+        return index;
+    }
+
+    @Override
+    public void nextFilter(RpcObject rpc, RemoteCall call, RpcSender sender){
+        int index = getAndIncrFilterIndex();
+        int size = filters.size();
+        if(index > size - 1){
+            throw new RuntimeException();
+        }
+        RpcFilter filter = filters.get(index);
+        filter.doFilter(rpc, call, sender, this);
+    }
+
+    public void addRpcFilter(RpcFilter filter){
+        filters.add(filter);
+    }
+
+    @Override
+    public void startFilter(RpcObject rpc, RemoteCall call, RpcSender sender){
+        try{
+            rpcFilterIndex.set(0);
+            this.nextFilter(rpc, call, sender);
+        }
+        finally{
+            rpcFilterIndex.remove();
+        }
+    }
+}
